929be5f3b1d01e42e27a9264af1a399af0fb3e2c
A. Mestra <89419093+uzideath@users.noreply.github.com>
2025-05-13 13:38:54 -0500

    Update closing.module.ts

--- src/closing/closing.module.ts
+++ src/closing/closing.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { ClosingController } from './closing.controller';
+import { ClosingService } from './closing.service';
 
 @Module({
-    providers: [PrismaService],
+    providers: [PrismaService, ClosingService],
     controllers: [ClosingController]
 })
 export class ClosingModule { }
