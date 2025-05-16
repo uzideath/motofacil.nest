9f0a5261468879a749481221f92407cb52658540
A. Mestra <89419093+uzideath@users.noreply.github.com>
2025-05-15 23:49:43 -0500

    wip

--- src/receipt/receipt.service.ts
+++ src/receipt/receipt.service.ts
@@ -18,9 +18,16 @@ export class ReceiptService {
 
     await page.setContent(html, { waitUntil: 'networkidle0' });
 
-   const pdfBuffer = await page.pdf({
+    const pdfBuffer = await page.pdf({
+      width: '80mm',
       printBackground: true,
       preferCSSPageSize: true,
+      margin: {
+        top: '0mm',
+        bottom: '0mm',
+        left: '0mm',
+        right: '0mm',
+      },
     });
 
 
